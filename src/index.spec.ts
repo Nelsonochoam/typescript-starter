7a5946989e0daa65340e8a8d35f0534543020a48
Nelson Ochoa <nelsonochoam@gmail.com>
2021-09-21 20:00:51 -0600

    cleaned up interview starter

---
+++ src/index.spec.ts
@@ -0,0 +1,5 @@
+describe("Some tests", () => {
+  it("add: Adds 2 numbers", () => {
+    expect(1+1).toBe(2)
+  })
+})
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { add, subtract } from './utils/math'
-
-function main() {
-  console.log(add(1, 1))
-  console.log(subtract(2, 2))
-}
-
-main()
